67ced21eb40888a7ec19636705f45f13ad8d0283
AlfredClark <alfredclark@163.com>
2026-05-23 15:17:17 +0800

    chore: Optimized scripts

--- version-bump.ts
+++ version-bump.ts
@@ -2,13 +2,25 @@ import { readFileSync, writeFileSync } from "fs";
 
 const targetVersion = process.env.npm_package_version ?? "0.0.0";
 
-const manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
+type Manifest = {
+  id: string;
+  name: string;
+  version: string;
+  minAppVersion: string;
+  description: string;
+  isDesktopOnly: boolean;
+  author: string;
+  authorUrl: string;
+  fundingUrl: string;
+};
+
+const manifest = JSON.parse(readFileSync("manifest.json", "utf8")) as Manifest;
 const { minAppVersion } = manifest;
 manifest.version = targetVersion;
 writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
 
-const versions = JSON.parse(readFileSync("versions.json", "utf8"));
-if (!Object.values(versions).includes(minAppVersion)) {
+const versions = JSON.parse(readFileSync("versions.json", "utf8")) as Record<string, string>;
+if (!Object.keys(versions).includes(targetVersion)) {
   versions[targetVersion] = minAppVersion;
   writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
 }
